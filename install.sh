945fe7755fbafcba78a77217c991e3f37634cc1a
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-09-11 12:08:37 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+sudo apt update
+sudo apt install -y python3 python3-pip
+pip install colorama
+python3 <(curl -Ls https://raw.githubusercontent.com/kalilovers/LightKnightBBR/main/bbr.py --ipv4)
