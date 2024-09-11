44b5bb7b69b9636d531755791b741932907b6ad6
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-09-11 12:18:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
+CYAN="\e[96m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+RED="\e[91m"
+BLUE="\e[94m"
+MAGENTA="\e[95m"
+WHITE="\e[97m"
+NC="\e[0m"
 
+# Function to check if a package is installed
+check_pkg() {
+    dpkg -l | grep -qw "$1"
+}
+
+echo && echo -e "$YELLOW Updating package list... $NC"
 sudo apt update
-sudo apt install -y python3 python3-pip
-pip install colorama
+
+# Check and install python3 if not installed
+if check_pkg "python3"; then
+    echo && echo -e "$GREEN Python3 is already installed. $NC"
+else
+    echo && echo -e "$YELLOW Installing Python3... $NC"
+    sudo apt install -y python3
+fi
+
+# Check and install python3-pip if not installed
+if check_pkg "python3-pip"; then
+    echo && echo -e "$GREEN pip3 is already installed. $NC"
+else
+    echo && echo -e "$YELLOW Installing pip3... $NC"
+    sudo apt install -y python3-pip
+fi
+
+# Check if colorama is installed in Python, and install if necessary
+if python3 -c "import colorama" &> /dev/null; then
+    echo && echo -e "$GREEN colorama is already installed. $NC"
+else
+    echo && echo -e "$YELLOW Installing colorama... $NC"
+    pip3 install colorama
+fi
+
+# Run the Python script
 python3 <(curl -Ls https://raw.githubusercontent.com/kalilovers/LightKnightBBR/main/bbr.py --ipv4)
